fe7e4590d4b314d0c48f79dfffef17e1dfe569df
flipnism <flipnism666@gmail.com>
2023-09-11 10:33:30 +0700

    fixing path and hashing?! maybe

--- __init__.py
+++ __init__.py
@@ -5,25 +5,32 @@ import hashlib
 comfy_path = os.path.dirname(folder_paths.__file__)
 js_path = os.path.join(comfy_path, "web")
 
-def sha256sum(filename):
-    with open(filename, 'rb', buffering=0) as f:
-        return hashlib.file_digest(f, 'sha256').hexdigest()
-def need_update():
-	return sha256sum('user.css')!=sha256sum(os.path.join(js_path,'user.css'))
 
+def hash_file(filename: str, blocksize: int = 4096) -> str:
+    hsh = hashlib.md5()
+    with open(filename, "rb") as f:
+        while True:
+            buf = f.read(blocksize)
+            if not buf:
+                break
+            hsh.update(buf)
+    return hsh.hexdigest()
+
+
+def need_update():
+    return hash_file(os.path.join(os.path.dirname(__file__), "user.css")) != hash_file(
+        os.path.join(js_path, "user.css")
+    )
 
 
 cur_css = ""
 if need_update():
-	print('updating user.css')
-	with open(os.path.join(os.path.dirname(__file__), "user.css")) as file:
-	    cur_css = file.read()
-	with open(os.path.join(js_path, "user.css"), "w") as f:
-	    f.write(cur_css)
-
-
-
-
+    print("updating user.css")
+    with open(os.path.join(os.path.dirname(__file__), "user.css")) as file:
+        cur_css = file.read()
+    with open(os.path.join(js_path, "user.css"), "w") as f:
+        f.write(cur_css)
 
 
 WEB_DIRECTORY = "./web"
+NODE_CLASS_MAPPINGS = {}
